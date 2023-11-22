43c1f9cd69ea7db1b7fda9d36617df7d546b6991
Enchanted <summerdayxumeng@gmail.com>
2023-11-22 21:38:31 +0800

    Enchanted-Shop -> 创建Maven聚合工程 & 添加相关Maven依赖

---
+++ Project-Java/Enchanted-Shop/Enchanted-Shop/EnchantedShop-gateway/src/main/java/com/enchanted/shop/Main.java
@@ -0,0 +1,15 @@
+package com.enchanted.shop;
+
+/**
+ * @ClassName: ${NAME}
+ * @Description: TODO
+ * @PackageName:com.enchanted.shop
+ * @Author Enchanted
+ * @Date 2023/11/22 21:16
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
